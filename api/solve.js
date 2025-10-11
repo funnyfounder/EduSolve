630ee4ecd397c97909ef9be4d72aeae83738e7ae
SURAJ KUMAR GUPTA <guptasuraj24680@gmail.com>
2025-10-12 01:14:46 +0530

    Update solve.js

--- api/solve.js
+++ api/solve.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
 
   try {
     const hfRes = await fetch(
-      'https://api-inference.huggingface.co/models/microsoft/Phi-4-mini-reasoning',
+      'https://api-inference.huggingface.co/models/microsoft/UserLM-8b',
       {
         method: 'POST',
         headers: {
